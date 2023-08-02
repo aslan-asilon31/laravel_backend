7eb77678557acb8f2c6c308e1e02c44ee78f739a
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2023-08-01 23:55:35 -0700

    update product

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class LandingPageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $products = Product::all();
+        return view('welcome', compact('products'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,25 @@
     
 
 
+    <table class="table table-bordered">
+      <thead>
+          <tr>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Price</th>
+          </tr>
+      </thead>
+      <tbody>
+          @foreach($products as $product)
+              <tr>
+                  <td>{{ $product->name }}</td>
+                  <td>{{ $product->image }}</td>
+                  <td>{{ $product->price }}</td>
+              </tr>
+          @endforeach
+      </tbody>
+  </table>
+
     
 
 
